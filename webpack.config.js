08ef95f60843c77ba8ce6ecaab9f9bc042d1654e
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-02-24 16:10:31 +0100

    Install and configure HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,4 +9,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "template.html",
+      filename: "index.html",
+    }),
+  ],
 };
